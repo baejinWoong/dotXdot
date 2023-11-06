939864a12b86f1b129192d00c155ac3f73f9b18a
배진웅 <jw.bae@elwide.com>
2023-11-06 18:34:06 +0900

    second commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import MainPageWrap from "../components/common/MainPageWrap";
-import { SketchPicker, Color, ColorResult } from "react-color";
+import { ChromePicker, ColorResult, RGBColor } from "react-color";
 import "../styles/mainPage.scss";
-import Modal from "../components/common/Modal";
+import styled from "styled-components";
+import { Kakao, LinkIcon } from "../images/svg";
 
 const xList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
@@ -13,34 +15,77 @@ interface x_content {
   xData: number;
 }
 
+const XcontentComponent = styled.div<{ $color: RGBColor }>`
+  background-color: rgba(
+    ${(props) =>
+      `${props.$color.r}, ${props.$color.g}, ${props.$color.b}, ${props.$color.a}`}
+  );
+`;
+
 const Xcontent = (props: x_content) => {
   const [isColorPicker, setIsColorPicker] = React.useState<boolean>(false);
-  const [color, setColor] = React.useState<Color>({
-    r: 0,
-    g: 0,
-    b: 0,
+  const [isWrightMessage, setIsWrightMessage] = React.useState<boolean>(false);
+  const [color, setColor] = React.useState<RGBColor>({
+    r: 50,
+    g: 17,
+    b: 15,
+    a: 0.1,
   });
 
+  const [userOverWrightcolor, setUserOverWrightcolor] =
+    React.useState<RGBColor>({
+      r: 50,
+      g: 17,
+      b: 15,
+      a: 1,
+    });
+
   const colorPickerChangeHandler = (color: ColorResult) => {
-    setColor(color.rgb);
+    setUserOverWrightcolor(color.rgb);
   };
 
   const openColorPickerHandler = () => {
     setIsColorPicker(true);
   };
 
-  const closeColorPickerHandler = () => {
-    console.log("click");
+  const messagePopupOpenHandler = () => {
     setIsColorPicker(false);
+    setIsWrightMessage(true);
   };
 
   return (
     <>
-      <div className="xContent" onClick={openColorPickerHandler}></div>
+      <XcontentComponent
+        className="xContent"
+        onClick={openColorPickerHandler}
+        $color={color}
+      />
       <Modal open={isColorPicker}>
-        <div className="popOver" onClick={closeColorPickerHandler}>
-          <SketchPicker color={color} onChange={colorPickerChangeHandler} />
-          <div>{isColorPicker ? "true" : "false"}</div>
+        <div className="contentPopupCard">
+          <ChromePicker
+            color={userOverWrightcolor}
+            onChange={colorPickerChangeHandler}
+          />
+          <div className="button wrap">
+            <button className="messageButton" onClick={messagePopupOpenHandler}>
+              메세지 남기기
+            </button>
+          </div>
+        </div>
+      </Modal>
+
+      <Modal open={isWrightMessage}>
+        <div className="contentPopupCard message">
+          <p>11.11데이 메세지를 전하세요</p>
+          <div className="inputBox">
+            <div className="inputWrap">
+              <input placeholder="받는 사람?" />
+            </div>
+          </div>
+          <textarea placeholder="보낼 사람 한 명쯤은 있잖아요? 😙 (placeholder)" />
+          <div className="button wrap">
+            <button className="messageButton">메세지 보내기</button>
+          </div>
         </div>
       </Modal>
     </>
@@ -66,9 +111,67 @@ const MainPage = () => {
           </div>
         ))}
       </div>
+      <div className="button wrap">
+        <button className="link">
+          <LinkIcon />
+          <span>링크복사</span>
+        </button>
+        <button className="kakao">
+          <Kakao />
+          <span>카카오톡 공유</span>
+        </button>
+      </div>
     </MainPageWrap>
   );
 };
 
 export default MainPage;
+
+interface I_modalProps {
+  open: boolean;
+  children?: React.ReactNode;
+}
+
+/**
+ * @param open isOpen Modal boolean, Default: false
+ */
+const Modal = ({ open, children }: I_modalProps) => {
+  React.useEffect(() => {
+    const isAnotherModal =
+      document.getElementsByClassName("modalWrap").length > 0;
+    const isHeader = document.body.getElementsByTagName("header").length > 0;
+    const isScroll = document.body.offsetHeight > window.innerHeight;
+    if (open) {
+      document.body.style.overflow = "hidden";
+      if (isHeader && isScroll) {
+        document.body.style.paddingRight = "15px";
+        // document.body.getElementsByTagName('header')[0].style.paddingRight = '55px'
+      }
+    } else if (!isAnotherModal) {
+      document.body.style.removeProperty("overflow");
+      if (isHeader && isScroll) {
+        document.body.style.removeProperty("padding-right");
+        document.body
+          .getElementsByTagName("header")[0]
+          .style.removeProperty("padding-right");
+      }
+    }
+  }, [open]);
+
+  if (open) {
+    const el = document.body;
+    return ReactDOM.createPortal(
+      <div
+        className="colorPickerWrap"
+        onClick={(event) => {
+          event.stopPropagation();
+        }}
+      >
+        {children}
+        <div className="dim" />
+      </div>,
+      el
+    );
+  } else return null;
+};
 
